0bb318fa9c5fb9147c37d1242b016894e3d7c80f
Sriram Pujar <srirampujar@gmail.com>
2025-12-14 14:48:42 +0530

    Enable PWA Service Worker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
+  // your next config
 };
 
-export default nextConfig;
+const withPWAConfig = withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+export default withPWAConfig(config);
