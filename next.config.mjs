3d5016717662846de3aa9a3330600afad38f9d11
Sriram Pujar <srirampujar@gmail.com>
2025-12-14 17:53:37 +0530

    Downgrade to Next 14 Stable

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import withPWA from "@ducanh2912/next-pwa";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: { ignoreDuringBuilds: true },
+    typescript: { ignoreBuildErrors: true },
+};
+
+export default withPWA({
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true,
+})(nextConfig);
